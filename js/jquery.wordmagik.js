0156de7a3804bc80906a35a0477d4ce6326eab89
Amyth <aroras.official@gmail.com>
2014-06-11 09:35:26 +0530

    Added the js plugin file with basic structure

---
+++ js/jquery.wordmagik.js
@@ -0,0 +1,21 @@
+/**
+ * jQuery Word Magik plugin
+ *
+ * Copyright (c) 2014 Amyth Arora
+ * Licensed under MIT License
+ *
+ * https://github.com/amyth/WordMagik
+ *
+ * VERSION: 0.0.1
+ *
+ */
+
+(function($) {
+	$.fn.wordmagik = function(options) {
+
+		// Defines default options for word magik plugin
+		var defaults = {};
+		var settings = $.extend({}, defaults, options);
+
+	});
+}(jQuery));
